ab5a748fd9ad9cbfc1451366379758738febb7e5
Nil Geisweiller <ngeiswei@gmail.com>
2023-01-26 08:51:40 +0200

    Add rebuild.sh

---
+++ rebuild.sh
@@ -0,0 +1,21 @@
+# Prepare environment
+rustup update stable
+pip install -U pip
+cargo install --force cbindgen
+pip install conan==1.57
+conan profile new --detect default
+pip install -e ./python[dev]
+
+# Build Hyperon library
+cd ./lib
+cargo build
+cargo test
+cargo doc --no-deps
+cd ..
+
+# Build C and Python API
+trash build; mkdir build; cd build
+cmake ..
+make
+make check
+cd ..
