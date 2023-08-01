0ccb063edddb63bc27310fbf70239ac639e82555
Nil Geisweiller <ngeiswei@gmail.com>
2023-08-01 09:17:25 +0300

    Bump  up conan version 1.60.2

--- rebuild.sh
+++ rebuild.sh
@@ -2,7 +2,7 @@
 rustup update stable
 pip install -U pip
 cargo install --force cbindgen
-pip install conan==1.60.1
+pip install conan==1.60.2
 conan profile new --detect default
 pip install pip==23.1.2
 
